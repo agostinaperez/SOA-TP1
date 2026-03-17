1fb427585c1f900bacfc975accbfa4ea98ef5504
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2026-03-17 11:40:12 -0300

    setup service

---
+++ order-audit/src/test/java/com/tp/kafka/order_audit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.kafka.order_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
